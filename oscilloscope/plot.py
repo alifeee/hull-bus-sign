59ed601abe98169ca5a58d8866310036d2a3f98a
alifeee <alifeeerenn@hotmail.com>
2023-08-11 17:10:46 +0100

    add python to plot oscilloscope data

---
+++ oscilloscope/plot.py
@@ -0,0 +1,42 @@
+import os
+import matplotlib.pyplot as plt
+import pandas
+
+FOLDER = "send-0x0e00_1-clock_2-data_0"
+file_dir = os.path.dirname(__file__)
+
+plt.style.use("dark_background")
+
+if __name__ == "__main__":
+    # import data with pandas csv
+    ch1_filename = os.path.join(file_dir, FOLDER, "CH1.csv")
+    ch2_filename = os.path.join(file_dir, FOLDER, "CH2.csv")
+    ch1_df = pandas.read_csv(ch1_filename, header=None)
+    ch2_df = pandas.read_csv(ch2_filename, header=None)
+
+    # the settings are the top 19 rows and the first 2 columns
+    ch1_settings_df = ch1_df.iloc[:19, :2]
+    # make into dictionary with the first column as the key
+    ch1_settings = dict(zip(ch1_settings_df.iloc[:, 0], ch1_settings_df.iloc[:, 1]))
+
+    ch2_settings_df = ch2_df.iloc[:19, :2]
+    ch2_settings = dict(zip(ch2_settings_df.iloc[:, 0], ch2_settings_df.iloc[:, 1]))
+
+    # the data is the rest of the rows
+    # the first column is the time
+    # the second column is the voltage
+
+    ch1_data_df = ch1_df.iloc[19:, -3:-1]
+    ch2_data_df = ch2_df.iloc[19:, -3:-1]
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212, sharex=ax1, sharey=ax1)
+
+    ax1.plot(ch1_data_df.iloc[:, 0], ch1_data_df.iloc[:, 1], color="yellow")
+    ax2.plot(ch2_data_df.iloc[:, 0], ch2_data_df.iloc[:, 1], color="blue")
+
+    ax1.set_ylabel("Voltage (V)")
+    ax2.set_ylabel("Voltage (V)")
+    ax2.set_xlabel("Time (s)")
+    plt.show()
